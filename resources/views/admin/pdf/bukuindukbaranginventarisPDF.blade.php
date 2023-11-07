34bd7b08a91ce89d29d66c436759fd70365f3e55
KhaznaKhalifatunesya <guineverearts@gmail.com>
2023-11-07 15:36:24 +0700

    layout pdf

---
+++ resources/views/admin/pdf/bukuindukbaranginventarisPDF.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h2 align="center">SMK... </h2>
+        <h2 align="center">BUKU INDUK BARANG INVENTARIS</h2>
+        <h4>Departemen Mesin Konvensi Energi</h4>
+
+        <table border="1">
+            <tr align="center">
+                <th>Nomor urut</th>
+                <th>Tanggal Pembukuan</th>
+                <th>Kode Barang</th>
+                <th>Nama Barang</th>
+                <th>Keterangan, Merk, Nomor Ukuran, dsb</th>
+                <th>Kuantitas</th>
+                <th>Nama Satuan</th>
+                <th>Tahun Pembuatan</th>
+                <th>Asal Barang</th>
+                <th>Tanggal Penyerahan/Perolehan</th>
+                <th>Keadaan Barang</th>
+                <th>Harga</th>
+                <th>Keterangan</th>
+            </tr>
+            <tr align="center">
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+            </tr>
+            <tr align="center">
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+                <td> . </td>
+            </tr>
+        </table>
+    </body>
+</html>
