d7577a6ba3fcc0e62e8dfca30b7dbcda7276d5fc
KhaznaKhalifatunesya <guineverearts@gmail.com>
2023-11-07 15:56:42 +0700

    layout table

--- resources/views/admin/pdf/kartustokPDF.blade.php
+++ resources/views/admin/pdf/kartustokPDF.blade.php
@@ -7,34 +7,34 @@
 
         <table border="1" cellpadding="7">
             <tr align="center" bgcolor="#e9e9e9">
-                <th rowspan="2">Tanggal</th>
-                <th rowspan="2">Deskripsi/Spesifikasi</th>
-                <th rowspan="2">Harga/Unit</th>
+                <th rowspan="2" style='width:30px'>Tanggal</th>
+                <th rowspan="2" style='width:300px'>Deskripsi/Spesifikasi</th>
+                <th rowspan="2" style='width:100px,'>Harga/Unit</th>
                 <th colspan="2">Keluar masuk barang</th>
-                <th rowspan="2">Sisa</th>
-                <th rowspan="2">Keterangan</th>
+                <th rowspan="2" style='width:50px'>Sisa</th>
+                <th rowspan="2" style='width:300px'>Keterangan</th>
             </tr>
             <tr align="center" bgcolor="#e9e9e9">
                 <th>Masuk</th>
                 <th>Keluar</th>
             </tr>
             <tr align="center">
-                <td> . </td>
-                <td> . </td>
-                <td> . </td>
-                <td> . </td>
-                <td> . </td>
-                <td> . </td>
-                <td> . </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
             <tr align="center">
-                <td> . </td>
-                <td> . </td>
-                <td> . </td>
-                <td> . </td>
-                <td> . </td>
-                <td> . </td>
-                <td> . </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
         </table>
     </body>
